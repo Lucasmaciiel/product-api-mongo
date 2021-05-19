6345a8ef45a2a5030d2e31309a6bccae1810391c
lucas.maciel <lucasmacielgois@gmail.com>
2021-05-19 15:42:56 -0300

    Corrige nome da Collection

--- src/main/java/com/desafiojavaspringboot/uol/api/model/Product.java
+++ src/main/java/com/desafiojavaspringboot/uol/api/model/Product.java
@@ -17,7 +17,7 @@ import java.io.Serializable;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Document(collection = "procuct")
+@Document(collection = "product")
 public class Product implements Serializable {
 
     private static final long serialVersionUID = 1L;
